a04bc5ba5ce791d692f87f04a245006336504dde
Evgen Kotlyarchuck <93705949+Evcat6@users.noreply.github.com>
2022-06-07 16:41:56 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,81 @@
+const prev = document.getElementById('btn-prev'),
+    next = document.getElementById('btn-next'),
+    slides = document.querySelectorAll('.slide'),
+    dots = document.querySelectorAll('.dot');
+
+
+
+let index = 0;
+
+const activeSlide = n => {
+    for (const slide of slides) {
+       slide.classList.remove('active'); 
+    }
+    slides[n].classList.add('active');
+};
+
+const nextSlide = () => {
+    if(index == slides.length - 1) {
+        index = 0;
+        prepareCurrentSlide(index);
+
+    } else {
+        index++;
+        prepareCurrentSlide(index);
+
+    }
+     
+
+};
+
+
+
+const prevSlide = () => {
+    if(index == 0 ) {
+        index = slides.length - 1;
+        prepareCurrentSlide(index);
+
+    } else {
+        index--;
+        prepareCurrentSlide(index);
+
+
+    }
+     
+
+};
+
+
+const activeDot = n => {
+    for (dot of dots) {
+       dot.classList.remove('active'); 
+    }
+    dots[n].classList.add('active');
+};
+
+
+
+//combine of functions//
+
+const prepareCurrentSlide = index => {
+    activeSlide(index);
+        activeDot(index);
+
+}
+
+dots.forEach((item, indexDot) => {
+    item.addEventListener('click', () => {
+        index= indexDot;
+        prepareCurrentSlide(index);
+    })
+});
+
+window.setInterval(nextSlide, 2000);
+
+
+
+
+
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
